e6a2731f52a7ef8b3b7fbf47d52f357965a29041
Te Taipo <hokioi-security@protonmail.ch>
2019-12-22 21:36:56 +1300

    Update test.php

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
     require_once( 'class_password_filter.php' );
     $PasswordFilter = new PasswordFilter();
     # choose from either bool, object, json or jbool (json boolean) for output type
-    var_dump( $PasswordFilter->pass_check( 'TestPasswordHere', 'jbool' ) );
+    var_dump( $PasswordFilter->pass_check( 'TestPassw0rdHere', 'object' ) );
 ?>
